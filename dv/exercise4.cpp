fd563e75c3d9b7e80deecc9597df3b35505214da
umanachi <123425184+umanachi@users.noreply.github.com>
2023-03-10 00:47:41 -0500

    completed lab 4 q4
    but there is a definitely a more efficient way to do it (if I have time -- will make a single function)

--- dv/exercise4.cpp
+++ dv/exercise4.cpp
@@ -1,6 +1,37 @@
 #include <catch2/catch_test_macros.hpp>
 #include <VExercise4.h>
+#include <cstdint>
+#include <stdlib.h>
 
-TEST_CASE("Exercise 4") {
+TEST_CASE("Sel: Don't Care") {
   VExercise4 model;
+  model.alpha = 0;
+  model.beta = 1;
+  model.gamma = 2;
+  model.cs = 0;
+  for (model.s = 0; model.s < 4; model.s++) {
+    model.eval();
+    REQUIRE(model.out == 0);
+  }
+}
+
+TEST_CASE("Sel: 0-2") {
+  VExercise4 model;
+  model.alpha = 0;
+  model.beta = 1;
+  model.gamma = 2;
+  model.cs = 0;
+  for (model.s = 0; model.s < 3; model.s++) {
+    model.eval();
+    REQUIRE(model.out == model.s);
+  }
+}
+
+TEST_CASE("Sel: 3"){
+  VExercise4 model;
+  model.alpha = 0;
+  model.beta = 1;
+  model.gamma = 2;
+  model.eval();
+  REQUIRE(model.out == (model.alpha & (model.beta | model.gamma)));
 }
